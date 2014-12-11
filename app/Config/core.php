38cfd7377370bfd51ce2617bd6d3b591653bbe18
yuriyua <yuriyuag@gmail.com>
2014-12-11 08:40:10 +0200

    change Security.salt and Security.cipherSeed values

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '7xZ*i5FK1jd8ZyQN)KN91b!Lb*#8pTqed%rlgwO#');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '49998827929417583602204622942822');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
